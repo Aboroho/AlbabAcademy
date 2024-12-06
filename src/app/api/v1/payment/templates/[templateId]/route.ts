849750c3f3bd6f1cd9e2c9f52b8e5eac9f4571b9
unknown <dev.rifat.sarker@gmail.com>
2024-12-07 03:52:03 +0600

    Fix: Ts issue

--- src/app/api/v1/payment/templates/[templateId]/route.ts
+++ src/app/api/v1/payment/templates/[templateId]/route.ts
@@ -3,7 +3,7 @@ import {
   authorizeAdmin,
 } from "@/app/api/middlewares/auth/auth_middlewares";
 import { withMiddleware } from "@/app/api/middlewares/withMiddleware";
-import { APIError } from "@/app/api/utils/handleError";
+
 import { apiResponse } from "@/app/api/utils/handleResponse";
 import { prismaQ } from "@/app/api/utils/prisma";
 
@@ -12,17 +12,7 @@ export const DELETE = withMiddleware(
   authorizeAdmin,
   async (_, { params }: { params: Promise<{ templateId: string }> }) => {
     const templateId = (await params).templateId;
-    const paymentRequest = await prismaQ.paymentRequest.findFirst({
-      where: {
-        payment_template_id: parseInt(templateId),
-      },
-    });
 
-    if (paymentRequest) {
-      throw new APIError(
-        "There are some payment request associated with this payment template. You can not delete this template."
-      );
-    }
     const status = await prismaQ.paymentTemplate.delete({
       where: {
         id: parseInt(templateId),
